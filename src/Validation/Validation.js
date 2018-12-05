90627a0ba8ff15d6bca604244c4954da9afc410e
Isaac Fonseca <isaacfo1010@gmail.com>
2018-12-05 12:06:58 -0500

    inital stuff for react u #2 assign

---
+++ src/Validation/Validation.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+
+const validation = (props) => {
+    return (
+        <div>
+            <p>Text too short!</p>
+        </div>
+    );
+};
+
+
+
+
+
+
+export default validation;
